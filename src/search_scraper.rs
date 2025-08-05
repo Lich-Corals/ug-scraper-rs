62d323b7b44638b120fc914ae339e9bda14bc5f4
Linus Tibert <git@linus.tibert.de>
2025-08-05 17:13:56 +0200

    add multi-page search

--- src/search_scraper.rs
+++ src/search_scraper.rs
@@ -20,13 +20,27 @@ use regex::{CaptureMatches, Regex};
 use std::str::FromStr;
 use ureq::{Error as ReqError};
 
-const VALID_LINK_REGEX: &str = r"https://www\.ultimate-guitar\.com/search\.php\?(?:[search_type=\w+]+&)*value=.+";
 const DATA_REGEX: &str = r"(?:&quot;id&quot;:(\d{2,}).+?song_id&quot;:(\d+).+?song_name&quot;:&quot;([^&]+).*?artist_name&quot;:&quot;([^&]+).*?type&quot;:&quot;([\w\s]+).+?votes&quot;:(\d*).*?rating&quot;:([\d\.]+)).*?&quot;tab_url&quot;:&quot;(https://tabs\.ultimate-guitar\.com/tab/[^/\.]+/[^/\.&]+)&quot;";
 const BASE_SEARCH_URL: &str = "https://www.ultimate-guitar.com/search.php?search_type=title&value=";
 
-pub fn get_search_results(query: &str) -> Result<Vec<SearchResult>, Error> {
-        let search_url: String = BASE_SEARCH_URL.to_string() + &encode_string(query);
-        validate_link(&search_url)?;
+pub fn get_search_results(query: &str, max_additional_pages: u8) -> Result<Vec<SearchResult>, Error> {
+        let mut results: Vec<SearchResult> = vec![];
+        for i in 1..max_additional_pages {
+                match search_page(query, i) {
+                        Ok(mut r) => {
+                                if r.len() == 0 {
+                                        return Ok(results)
+                                }
+                                results.append(&mut r);
+                        },
+                        Err(e) => return Err(e)
+                }
+        }
+        Ok(results)
+}
+
+fn search_page(query: &str, i: u8) -> Result<Vec<SearchResult>, Error> {
+        let search_url: String = BASE_SEARCH_URL.to_string() + &encode_string(query) + "&page=" + &i.to_string();
         let raw_html: String;
         match get_raw_html(&search_url) {
                 Ok(d) => raw_html = d,
@@ -38,7 +52,6 @@ pub fn get_search_results(query: &str) -> Result<Vec<SearchResult>, Error> {
                         _ => return Err(Error::RequestError(e.to_string())),
                 },
         }
-
         let regex = Regex::new(DATA_REGEX).unwrap();
         let captures = regex.captures_iter(&raw_html);
         match unwrap_results(captures) {
@@ -64,42 +77,20 @@ fn unwrap_results(matches: CaptureMatches) -> Result<Vec<SearchResult>, Box<dyn 
         Ok(results)
 }
 
-fn validate_link(url: &str) -> Result<(), Error> {
-        let regex = Regex::new(VALID_LINK_REGEX).unwrap();
-        let captures = regex.captures(url);
-        match captures {
-                Some(_d) => Ok(()),
-                None => Err(Error::InvalidURL),
-        }
-        
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::search_scraper::{get_search_results, validate_link};
+    use std::u8;
 
-        #[test]
-        fn search_link_validation() {
-                let valid_links: Vec<&str> = vec!["https://www.ultimate-guitar.com/search.php?search_type=title&value=never%20gonna%20give%20you%20up#",
-                        "https://www.ultimate-guitar.com/search.php?value=never%20gonna%20give%20you%20up#"];
-                let invalid_links: Vec<&str> = vec!["https://www.ultimate-guitar.com/search.php"];
-                for link in valid_links {
-                        assert_eq!(validate_link(link), Ok(()));
-                }
-                for link in invalid_links {
-                        assert_ne!(validate_link(link), Ok(()));
-                }
-        }
-
+    use crate::search_scraper::{get_search_results};
         #[test]
         fn search_results() {
-                let valid_search_queries: Vec<&str> = vec!["NEVER GONNA GIVE YOU UP", "let it be", "Don't stop me now"];
+                let valid_search_queries: Vec<&str> = vec!["NEVER GONNA GIVE you up", "Don't stop me now"];
                 for query in valid_search_queries {
-                        assert!(get_search_results(query).unwrap().len() >= 1);
+                        assert!(get_search_results(query, u8::MAX).unwrap().len() >= 1);
                 }
                 let no_result_queries: Vec<&str> = vec!["this should_not return any #results!"];
                 for query in no_result_queries {
-                        assert!(get_search_results(query).unwrap().len() == 0);
+                        assert!(get_search_results(query, 1).unwrap().len() == 0);
                 }
         }
 }
