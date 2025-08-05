7ab24438ce6ace770ada485c3daf1876beed89c3
Linus Tibert <git@linus.tibert.de>
2025-08-05 19:13:05 +0200

    add MIT licence

--- src/search_scraper.rs
+++ src/search_scraper.rs
@@ -1,18 +1,8 @@
 // UG-Tab-Scraper - A basic rust API for getting data from Ultimate Guitar
 // Copyright (C) 2025  Linus Tibert
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public Licence as published
-// by the Free Software Foundation, either version 3 of the Licence, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public Licence for more details.
-//
-// You should have received a copy of the GNU Affero General Public Licence
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// This program was originally published under the MIT licence as seen
+// here: https://github.com/Lich-Corals/ug-tab-scraper-rs/blob/mistress/LICENSE
 
 use crate::types_and_constants::*;
 use crate::network::{encode_string, get_raw_html};
